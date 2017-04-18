0debb6b3839d3a30eb0a948b651316978c451dec
Chloe Choi <chloe.meowc@gmail.com>
2017-04-18 17:18:32 -0400

    feat: fix order model's table name from order to orders

--- db/models/order.js
+++ db/models/order.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 const db = require('./index.js')
 
 
-module.exports = db => db.define('order', {
+module.exports = db => db.define('orders', {
   status: {
     type: Seqelize.ENUM('Pending','Complete'),
     allowNull: false
