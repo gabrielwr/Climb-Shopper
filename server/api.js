7dac2e22702a9c012b9ab363c14f494293e53b56
Gabriel <gabriel.wrowe@gmail.com>
2017-04-19 15:52:39 -0400

    add review api

--- server/api.js
+++ server/api.js
@@ -6,6 +6,7 @@ api
   .get('/heartbeat', (req, res) => res.send({ok: true}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
+  .use('/reviews', require('./review'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
