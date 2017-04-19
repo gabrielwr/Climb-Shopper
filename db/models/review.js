cda53a59d65d3b926b33b4e85c6b4ba51f49d0a4
Chloe Choi <chloe.meowc@gmail.com>
2017-04-18 21:15:42 -0400

    feat: create review model

--- db/models/review.js
+++ db/models/review.js
@@ -1,19 +1,23 @@
+'use strict'
+const Sequelize = require('sequelize')
+const db = require('./index.js')
+
 module.exports = db => db.define('reviews', {
-  title: STRING,
-  content: TEXT,
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
   num_stars: {
-    type: FLOAT,
+    type: Sequelize.DECIMAL(1, 2),
+    allowNull: false,
     validate: {
-      min: 1, max: 5
+      is: /^\d{1}\.\d{0,1}$/,
+      min: 0,
+      max: 5
     }
   }
-},{
-  //  getterMethods: {
-  //    type: () => {
-  //
-  //    }
-  //  }
-  //
-  //
-  // }
-)
+})
