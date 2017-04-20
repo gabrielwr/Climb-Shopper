5bc7b09623d889fb85f8743bd124defdbc561f32
Chloe Choi <chloe.meowc@gmail.com>
2017-04-20 11:27:21 -0400

    fix: add mustBeAdmin to product api

--- server/product.js
+++ server/product.js
@@ -1,28 +1,27 @@
 'use strict'
 const db = require('APP/db')
 const Product = db.model('Products')
-const { mustBeLoggedIn, forbidden } = require('./auth.filters')
+const { forbidden, mustBeAdmin } = require('./auth.filters')
 
 module.exports = require('express').Router()
   .get('/',
-    forbidden('listing Products is not allowed'),
     (req, res, next) =>
       Product.findAll()
         .then(products => res.json(products))
         .catch(next))
   .post('/',
+    mustBeAdmin,
     (req, res, next) =>
       Product.create(req.body)
         .then(product => res.status(201).json(product))
         .catch(next))
   .get('/:id',
-    mustBeLoggedIn,
     (req, res, next) =>
       Product.findById(req.params.id)
         .then(product => res.json(product))
         .catch(next))
   .put('/:id',
-    mustBeLoggedIn,
+    mustBeAdmin,
     (req, res, next) =>
       Product.findById(req.params.id)
         .then(product => product.update(req.body))
@@ -30,7 +29,7 @@ module.exports = require('express').Router()
         .catch(next))
   .delete('/:id',
     // TO DO: make sure that this user is Admin
-    mustBeLoggedIn,
+    mustBeAdmin,
     (req, res, next) =>
       Product.findById(req.params.id)
         .then(product => product.destroy())
