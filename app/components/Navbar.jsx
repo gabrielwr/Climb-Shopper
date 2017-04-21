d9459552889961f6a811a071a732c2cdf393d7b3
Student <jeff.kandel@gmail.com>
2017-04-21 11:05:36 -0400

    fix: add WhoAmI to Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link, browserHistory } from 'react-router'
 import Login from './Login'
+import WhoAmI from './WhoAmI'
 
 /* -----------------    COMPONENT     ------------------ */
 
