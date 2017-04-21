cafb7299deef3cf1da7ff895d7d26f8e6eceb904
Chloe Choi <chloe.meowc@gmail.com>
2017-04-21 11:49:29 -0400

    feat: skeletons of cart reducer and component

--- app/reducers/cart.jsx.js
+++ app/reducers/cart.jsx.js
@@ -1,22 +1,60 @@
 import axios from 'axios';
 
 const initialState = {
-    students: [],
-    selectedStudent: {}
+    pastOrders: [],
+    currentOrder: {}
   }
 
 
 /* -----------------    ACTION TYPES     ------------------ */
-const UPDATE_STUDENT = 'UPDATE_STUDENT';
+const CREATE_ORDER = 'CREATE_ORDER'
+const SET_CURRENT_ORDER = 'SET_CURRENT_ORDER'
+const ADD_PRODUCT_TO_ORDER = 'ADD_PRODUCT_TO_ORDER'
+const UPDATE_PRODUCT_IN_ORDER = 'UPDATE_PRODUCT_IN_ORDER'
+const REMOVE_PRODUCT_FROM_ORDER = 'REMOVE_PRODUCT_FROM_ORDER'
+const COMPLETE_ORDER = 'COMPLETE_ORDER'
+const SET_PAST_ORDER = 'SET_PAST_ORDER'
 
 
 /* ------------   ACTION CREATORS     ------------------ */
 
-export const createStudent = (student) => ({
-  type: CREATE_STUDENT,
-  student: student
+export const createOrder = (order) => ({
+  type: CREATE_ORDER,
+  currentOrder: order
+})
+
+export const setCurrentOrder = (order) => ({
+  type: SET_CURRENT_ORDER,
+  currentOrder: order
+})
+
+export const updateProductInOrder = (productId, price, quantity) => ({
+  type: UPDATE_PRODUCT_IN_ORDER,
+  productId: productId,
+  price: price,
+  quantity: quantity
+})
+
+export const addProductToOrder = (productId) => ({
+  type: ADD_PRODUCT_TO_ORDER,
+  productId: productId
+})
+
+export const removeProductFromOrder = (productId) => ({
+  type: REMOVE_PRODUCT_FROM_ORDER,
+  productId: productId
+})
+
+export const completeOrder = () => ({
+  type: COMPLETE_ORDER,
+})
+
+export const setPastOrder = (orders) => ({
+  type: SET_PAST_ORDER,
+  pastOrders: orders
 })
 /* ------------       REDUCERS     ------------------ */
+
 
 /* ------------       DISPATCHERS     ------------------ */
 
