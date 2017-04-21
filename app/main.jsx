b4f468140fe67f50fe33f753b6169cc882605430
Student <jeff.kandel@gmail.com>
2017-04-21 12:16:04 -0400

    fix: fix router after weird mergE

--- app/main.jsx
+++ app/main.jsx
@@ -3,46 +3,25 @@ import React from 'react'
 import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
-
 import store from './store'
 import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import NotFound from './components/NotFound'
-<<<<<<< HEAD
 import AllProducts from './components/AllProducts'
-=======
 import Root from './components/Root'
->>>>>>> master
 import Authenticate from './components/Authenticate'
 
-
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth }),
-
-)(
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
-      </nav>
-      {children}
-    </div>
-)
 
 const EmptyApp = connect(
   ({ }) => ({ })
 )(
-  ({ children}) =>
-    <div>Hey there, replace with me an actual root element
-    {children}
-    </div>
-
-  ({children }) =>
-    <div>Hey there, hows it going whatcha doin
-    {children}
+  ({ }) =>
+    <div>
     </div>
 )
+
+
 
 const allProductsOnEnter = () => {
   // axios.get('/api')
@@ -51,12 +30,10 @@ const allProductsOnEnter = () => {
 render(
   <Provider store={ store }>
     <Router history={ browserHistory }>
-      <Route path="/" component={ EmptyApp }>
-
+      <Route path="/" component={ Root }>
         <Route path="/products" onEnter={ allProductsOnEnter } component={ AllProducts } />
         {/*products/add is an admin only view*/}
         <Route path="/products/add" component={ EmptyApp } />
-      <Route path="/" component={ Root }>
         <Route path="/products" component={ EmptyApp } />
         <Route path="/products/:id" component={ EmptyApp } />
         <Route path="/users" component={ EmptyApp } />
