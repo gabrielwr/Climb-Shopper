72025af672c6a72366fbcb1df1a73f650b21731a
Chloe Choi <chloe.meowc@gmail.com>
2017-04-21 16:39:58 -0400

    feat: add actions on order reducer

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
   auth: require('./auth').default,
-  order: require('./order').default
+  order: require('./order').default,
   product: require('./product').default,
   review: require('./review').default,
 })
