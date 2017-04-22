ba938eb58de28ce11ad4fcb93835987f9f6c5837
Gabriel <gabriel.wrowe@gmail.com>
2017-04-22 18:33:26 -0400

    add home carousel

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,20 +4,22 @@ import { login } from 'APP/app/reducers/auth'
 import { connect } from 'react-redux'
 
 const Home = (props) => (
-  <div>
-    <div className = "col-md-6">
-      <div className="carousel carousel-slider">
-        <a className="carousel-item" href="#one!">
-          <img src='hardcode here' />
-          <img src='hardcode here' />
-          <img src='hardcode here' />
-          <img src='hardcode here' />
-        </a>
+    <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+      <div className="carousel-inner" role="listbox">
+        <div className="carousel-item active">
+          <img className="d-block img-fluid" src="http://www.lockedcog.com/uploads/2015/03/dylan-mash.jpg" alt="First slide" />
+        </div>
+        <div className="carousel-item">
+          <img className="d-block img-fluid" src="http://www.lockedcog.com/uploads/2015/03/dylan-mash.jpg" alt="Second slide" />
+        </div>
+        <div className="carousel-item">
+          <img className="d-block img-fluid" src="http://www.mashsf.com/wp-content/uploads/2013/06/2T1A1500.jpg" alt="Third slide" />
+        </div>
       </div>
     </div>
-  </div>
 )
 
+export default Home
 // export default connect(
 //   state => (),
 //   {}
