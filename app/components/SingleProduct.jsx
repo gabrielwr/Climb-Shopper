4ece9a232e364f5923f9b26f6d198c4e76da1597
Chloe Choi <chloe.meowc@gmail.com>
2017-04-22 12:48:16 -0400

    feat: create single product component

--- app/components/SingleProduct.jsx
+++ app/components/SingleProduct.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
-
 import { login } from 'APP/app/reducers/auth'
 import { connect } from 'react-redux'
 
-const SingleProduct = (props) => (
+/*------------------- COMPONENT -----------------*/
+
+class SingleProduct extends React.Component (
+  constructor(props) {
+    super(props)
+    // this.state = {
+    //   story: {
+    //     name: '',
+    //     category: '',
+    //     price: 0,
+    //     images: [],
+    //     color: '',
+    //     size: '',
+    //     quantity: 0,
+    //     reviewStars:0.0,
+    //     description: ''
+    //   }
+    }
+  }
+
+  render() {
+    const story = this.state.story
+    if (!story) return <div/>
+    const
+  }
+
+
   <div>
-    <h2>Product name</h2>
-    <h3>Rest of the product description</h3>
+    <h2>{ this.props.selectedProduct.name }</h2>
+    <h3>  </h3>
   </div>
 
 )
+
+
+
+/*------------------- CONTAINER -----------------*/
 
 export default connect(
   state => ({singleProduct: state.selectedProduct}),
