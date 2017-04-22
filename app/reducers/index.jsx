f67b3a0ae8525812236faf0d5f952233ccf3d6dc
Gabriel <gabriel.wrowe@gmail.com>
2017-04-21 20:33:48 -0400

    add all files currently planned/necessary as reducers

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -4,6 +4,9 @@ const rootReducer = combineReducers({
   auth: require('./auth').default,
   product: require('./product').default,
   review: require('./review').default,
+  order: require('./review').default,
+  user: require('./user').default,
+  account: require('./account').default
 })
 
 export default rootReducer
