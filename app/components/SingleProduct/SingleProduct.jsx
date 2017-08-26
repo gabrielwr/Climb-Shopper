77c6a0b97dd0b18c0c75a7a9f15e2ad66f5f1301
Gabriel <gabriel.wrowe@gmail.com>
2017-08-26 15:23:52 -0400

    feat: update structure of single product

--- app/components/SingleProduct/SingleProduct.jsx
+++ app/components/SingleProduct/SingleProduct.jsx
@@ -16,12 +16,37 @@ export class SingleProduct extends React.Component {
     const reviews = this.props.reviews
     console.log('product is:', product)
     return (
-      <div>
-        <h2>{ product.name }</h2>
-        <p>Price: { formatPrice(product.price) }</p>
-        <div><p>{ product.description }</p></div>
-        <button>ADD TO CART</button>
-      </div>
+      <article>
+        <header>
+          <h1>{product.name}</h1>
+          <h2>{product.description}</h2>
+          <div>
+            <price>{formatPrice(product.price)}</price>
+            {/* {(orderItems &&
+              orderItems.filter(i => i.product_id === product.id).length)
+              ?<SingleProductButton
+              iconName='remove'
+              handleClick={this.deleteItemFromDatabase.bind(this, item.product_id)}
+              text='Remove'
+              />
+              :<SingleProductButton
+              iconName='shopping-cart'
+              handleClick={this.addItemToOrder.bind(this, item)}
+              text='Add to Cart'
+              />
+            } */}
+
+          </div>
+        </header>
+        <div>
+          { product.image &&
+            <img
+              src={`/images/Products/${product.image}`}
+              alt={product.name}
+            />
+          }
+        </div>
+      </article>
     )
   }
 }
