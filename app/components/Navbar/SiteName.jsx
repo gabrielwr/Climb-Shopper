722dada49f6dcb59c5066ee73628fe9eb31592d4
Gabriel <gabriel.wrowe@gmail.com>
2017-09-22 16:38:55 -0500

    chore: delete dead code and update SiteName text

--- app/components/Navbar/SiteName.jsx
+++ app/components/Navbar/SiteName.jsx
@@ -3,47 +3,23 @@ import { Link } from 'react-router'
 import styled from 'styled-components'
 
 /* -----------------    STYLED COMPONENTS    ------------------ */
-const Div = styled.div`
-  display: flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  color: ${ props => props.theme.text ? props.theme.text : 'black' };
-  min-height: ${ props => props.theme.height ? props.theme.height + 'px' : '50px' };
-`
-
 const NavLink = styled(Link)`
   display: flex;
-  flex-wrap: nowrap;
   align-items: center;
   color: black;
-  padding: .1rem;
+  padding-left: 2rem;
   min-height: ${ props => props.theme.height ? props.theme.height + 'px' : '50px' };
   text-decoration: none;
   &:hover {
-    background-color: white;
+    color: lightblue;
   }
 `
 
-const Anchor = styled.a`
-  text-decoration: none;
-  color: black;
-  &:hover {
-    background-color: white;
-  }
-`
 /* -----------------    COMPONENT     ------------------ */
 const SiteName = () => (
-  <Div>
-    <NavLink to="/">
-      Climb Shopper
-    </NavLink>
-    <div>
-      <small>
-        &nbsp;styled with
-        <Anchor href='https://www.styled-components.com/'> Styled-Components</Anchor>
-      </small>
-    </div>
-  </Div>
+  <NavLink to="/">
+    Climb Shopper
+  </NavLink>
 )
 
 export default SiteName;
--- app/components/Navbar/StyledNavigation/StyledNav.jsx
+++ app/components/Navbar/StyledNavigation/StyledNav.jsx
@@ -5,15 +5,16 @@ import styled from 'styled-components'
 export default styled.nav`
   display: flex;
   flex-wrap: nowrap;
-  background-color: lightgrey;
+  background-color: white;
   justify-content: space-between;
   color: ${ props => props.theme.text ? props.theme.text : 'white' };
-  min-height: ${ props => props.theme.height ? props.theme.height + 'px' : '50px' };
+  min-height: ${ props => props.theme.height ? props.theme.height + 'px' : '.5rem' };
 
   @media (max-width: 768px) {
     position: relative;
     flex-direction: column;
-    align-items: flex-start;
+    justify-content: center;
+    align-items: center;
     min-width: 200px;
     min-height: 100%;
   }
